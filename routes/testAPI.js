1ed015f752013a748f031c1ce40f039bc4da6450
Louis Richards <llouis.richards@gmail.com>
2023-07-03 14:04:47 +0200

    DB credential to dotenv

--- routes/testAPI.js
+++ routes/testAPI.js
@@ -1,20 +1,28 @@
 var express = require("express");
 var router = express.Router();
+require('dotenv').config();
 
 const mariadb = require('mariadb');
 const pool = mariadb.createPool({
-    host: 'localhost',
-    user: 'user',
-    password: 'user',
-    database: 'TestWebcand'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    connectionLimit: 5, // Adjust as per your requirements
 });
 
 /* Return "API is working properly" if connection
 to mariadb is successful */
 router.get("/", function(req, res, next) {
+    console.log(process.env.DB_HOST)
+    console.log(process.env.DB_PORT)
+    console.log(process.env.DB_USER)
+    console.log(process.env.DB_PASSWORD)
+    console.log(process.env.DB_NAME)
     pool.getConnection()
         .then(conn => {
-            conn.query("SELECT * FROM users")
+            conn.query("SELECT * FROM temptest;")
                 .then((rows) => {
                     res.send("API is working properly");
                     conn.end();
--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,23 @@
 var express = require('express');
 var router = express.Router();
+require('dotenv').config();
 
 const mariadb = require('mariadb');
 const pool = mariadb.createPool({
-    host: 'localhost',
-    user: 'user',
-    password: 'user',
-    database: 'TestWebcand'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    connectionLimit: 5, // Adjust as per your requirements
 });
-
 
 
 /* GET users listing using mariadb */
 router.get('/', function (req, res, next) {
     pool.getConnection()
         .then(conn => {
-            conn.query("SELECT * FROM users;")
+            conn.query("SELECT * FROM temptest;")
                 .then((rows) => {
                     res.send(rows);
                     conn.end();
